7c3d647101bfb7d4d26f501befc1640799cd50fe
Matija Kejzar <matija.kejzar@marand.si>
2019-06-24 23:15:38 +0200

    Publicly exposed new_uci_engine_stream()

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate tokio;
 extern crate tokio_codec;
 extern crate vampirc_uci;
 
+pub use crate::io::new_uci_engine_stream;
 pub use crate::io::run;
 pub use crate::io::run_engine;
 pub use crate::io::UciEngineStream;
