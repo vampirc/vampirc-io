ca0dc97f7d5d155975be9f1fd8e646980d3e122f
Matija Kejzar <matija.kejzar@marand.si>
2019-08-27 00:03:52 +0200

    Trying another approach with romio.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
 #[cfg(test)]
-extern crate bytes;
-extern crate crossbeam;
-extern crate futures;
-extern crate tokio;
-extern crate tokio_codec;
+extern crate romio;
 extern crate vampirc_uci;
 
-pub use crate::io::new_uci_engine_stream;
-pub use crate::io::run;
-pub use crate::io::run_engine;
-pub use crate::io::UciEngineStream;
-pub use crate::io::UciStream;
-
-mod codec;
 mod io;
-mod handler;
 
 #[cfg(test)]
 mod tests {
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,6 @@
 use std::io;
 
-use tokio::codec::{FramedRead, LinesCodec};
-use tokio::io::stdin;
-use tokio::prelude::{Future, Stream};
 use vampirc_uci::UciMessage;
-
-use vampirc_io::run_engine;
 
 pub fn setup() {
     println!("Setting up fixtures");
@@ -23,32 +18,24 @@ fn sum_test() {
 #[test]
 #[ignore]
 fn test_interactive_stdin_read_async() {
-    println!("Enter some input >>> ");
-
-    let frs = FramedRead::new(stdin(), LinesCodec::new());
-    let proc = frs.for_each(|m| {
-        println!("Message: {}", m);
-        Ok(())
-    })
-        .map_err(|e| {
-            println!("ERROR: {}", e);
-        })
-
-        ;
-
-    tokio::run(proc);
+//    println!("Enter some input >>> ");
+//
+//    let frs = FramedRead::new(stdin(), LinesCodec::new());
+//    let proc = frs.for_each(|m| {
+//        println!("Message: {}", m);
+//        Ok(())
+//    })
+//        .map_err(|e| {
+//            println!("ERROR: {}", e);
+//        })
+//
+//        ;
+//
+//    tokio::run(proc);
 }
 
 #[test]
 #[ignore]
 fn test_pass_func() {
-    run_engine(callback1, err_callback);
-}
-
-fn callback1(m: &UciMessage) {
-    println!("M: {}", *m);
-}
-
-fn err_callback(e: &io::Error) {
-    println!("Error handling: {}", e);
+    // run_engine(callback1, err_callback);
 }
