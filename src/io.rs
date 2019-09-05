abb0565544bfad88015f544e80d912b235e39022
Matija Kejzar <matija.kejzar@marand.si>
2019-09-06 00:11:12 +0200

    Return Stream stdin implementation.

--- src/io.rs
+++ src/io.rs
@@ -1,10 +1,15 @@
-use std::error::Error;
+use std::ops::DerefMut;
+use std::sync::Arc;
 
 use async_std::io;
+use async_std::io::{BufRead, BufReader};
+use async_std::sync::{Mutex, RwLock};
 use async_std::task::block_on;
+use futures::{FutureExt, join, SinkExt, Stream, StreamExt, TryFutureExt, TryStreamExt};
 use futures::channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
-use futures::join;
 use vampirc_uci::{ByteVecUciMessage, parse_with_unknown, Serializable, UciMessage};
+
+pub type UciStream = dyn Stream<Item=Result<UciMessage, io::Error>> + Unpin + Send + Sync;
 
 pub async fn run_stdin() -> io::Result<UnboundedReceiver<UciMessage>> {
     let (tx, rx) = unbounded::<UciMessage>();
@@ -33,29 +38,29 @@ pub async fn run_stdin_loop(tx: UnboundedSender<UciMessage>) -> io::Result<()> {
     Ok(())
 }
 
+pub fn stdin_msg_stream() -> Box<UciStream> {
+    let br = BufReader::new(io::stdin());
+    let stream = br.lines()
+        .map_ok(|line| parse_with_unknown(line.as_str()))
+        .map_ok(|msg_list| msg_list[0].clone())
+        ;
+    Box::new(stream)
+}
+
 
 
 #[cfg(test)]
 mod tests {
-    use futures::{StreamExt, TryStreamExt};
-
     use super::*;
-
-    async fn process_rx(mut rx: UnboundedReceiver<UciMessage>) {
-        loop {
-            let m: UciMessage = rx.next().await.unwrap();
-            println!("HERE WE ARE PROCESS: {}", m);
-        }
-
-    }
 
     #[test]
     fn test_run_in_loop() {
-        let (tx, mut rx) = unbounded();
         block_on(async {
-            let r = run_stdin_loop(tx);
-            let p = process_rx(rx);
-            join!(p, r);
+            let mut msg_stream = stdin_msg_stream();
+            while let Ok(msg_opt) = msg_stream.try_next().await {
+                let msg = msg_opt.unwrap();
+                println!("MSG RECEIVED VIA STREAM: {}", msg);
+            }
         });
     }
 
